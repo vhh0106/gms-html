545fa7803a129c5df8c18dfdc62bdf6285e34654
vhh0106 <vhh0106@gmail.com>
2024-01-22 01:26:48 +0700

    update

--- src/app/feature-login/feature-login.module.ts
+++ src/app/feature-login/feature-login.module.ts
@@ -1,28 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoginComponent} from "./login/login.component";
 import {RouterModule} from "@angular/router";
 import {NzIconModule} from "ng-zorro-antd/icon";
 import {NzInputModule} from "ng-zorro-antd/input";
 import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: LoginComponent,
-                data: {breadcrumb: 'login'},
-            }
-        ]),
-        NzIconModule,
-        NzInputModule,
-        NzCheckboxModule,
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: LoginComponent,
+        data: {breadcrumb: 'login'},
+      }
+    ]),
+    NzIconModule,
+    NzInputModule,
+    NzCheckboxModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+  ],
   declarations: [
     LoginComponent
   ]
